5d244dd18f9f992de283e7733056cb6b48ce8de4
Evtim Kostadinov <evtim.kostadinov2000@gmail.com>
2026-05-10 16:38:10 +0200

    add test floor

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,25 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
-        .add_systems(Startup, (setup_camera, setup_test))
+        .add_systems(Startup, (setup_camera, setup_test_floor, setup_test))
         .run();
 }
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d::default());
+}
+
+fn setup_test_floor(mut commands: Commands) {
+    commands.spawn((
+        RigidBody::Static,
+        Collider::rectangle(800.0, 20.0),
+        Transform::from_xyz(0.0, -200.0, 0.0),
+        Sprite {
+            color: Color::srgb(0.3, 0.3, 0.3),
+            custom_size: Some(Vec2::new(800.0, 20.0)),
+            ..default()
+        },
+    ));
 }
 fn setup_test(mut commands: Commands) {
     commands.spawn((
