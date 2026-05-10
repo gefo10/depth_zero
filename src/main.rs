18d7706e45d9cccff233c6697cd3be47883dce77
Evtim Kostadinov <evtim.kostadinov2000@gmail.com>
2026-05-10 16:35:20 +0200

    setup camera and check physics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use avian2d::prelude::*;
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
+        .add_systems(Startup, (setup_camera, setup_test))
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2d::default());
+}
+fn setup_test(mut commands: Commands) {
+    commands.spawn((
+        RigidBody::Dynamic,
+        Collider::rectangle(20.0, 60.0),
+        Transform::from_xyz(0.0, 3.0, 0.0),
+        Sprite {
+            color: Color::WHITE,
+            custom_size: Some(Vec2::new(20.0, 60.0)),
+            ..default()
+        },
+    ));
 }
